ceef4fcaa666318a6ccce0467ede8da3604a6290
zeynab nazari <zeynabnazari912@gmail.com>
2025-12-16 17:25:36 +0400

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
-
+import 'package:assets_audio_player/assets_audio_player.dart';
 void main() {
   runApp(const XylophoneApp());
 }
-
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey[500],),
+        backgroundColor: Colors.grey[500],
+      body: Center(
+        child: GestureDetector(
+          onTap: (){
+            AssetsAudioPlayer.newPlayer().open(
+              Audio("assets/audio/B.wav"),
+            );
+          },
+          child: Text("Click Me!",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+      ),
     );
   }
 }
